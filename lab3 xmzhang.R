67347b65a873205f05d924431dd31dfd8a584cd5
Xiangmei21 <xmzhang@iastate.edu>
2017-02-17 02:31:13 -0600

    This looks better

--- lab3 xmzhang.R
+++ lab3 xmzhang.R
@@ -12,69 +12,33 @@ d1_sheet[[i]] <- readxl::read_excel("Spreadsheets/FileOne.xlsx",sheet=i)
 d2_sheet[[i]] <- readxl::read_excel("Spreadsheets/FileTwo.xlsx",sheet=i) 
 colnames(d1_sheet[[i]])[1:2] <- c("ID","Time")  # fill empty colnames
 colnames(d2_sheet[[i]])[1:2] <- c("ID","Time")
-d2_sheet[[i]] <-d2_sheet[[i]] %>% mutate(test=2)
-d1_sheet[[i]] <-d1_sheet[[i]] %>% mutate(test=1)  ## specify the test for each file
+d2_sheet[[i]] <-d2_sheet[[i]] %>% mutate(test=2,sem=i)
+d2_sheet[[i]] = d2_sheet[[i]][names(d2_sheet[[i]])!=""] ## remove empty columns
+d1_sheet[[i]] <-d1_sheet[[i]] %>% mutate(test=1,sem=i) 
+d1_sheet[[i]] = d1_sheet[[i]][names(d1_sheet[[i]])!=""]
+## specify the test for each file
 } 
 
-## left join by same id for two files
-sem=NULL
-for (i in 1:4){
-sem[[i]]= left_join(d1_sheet[[i]],d2_sheet[[i]],by="ID") %>% 
-  mutate(sem=i) ## specify the semester
-sem[[i]]=sem[[i]][names(sem[[i]])!=""] ## remove empty columns
-}
-### sem1 miss the column "MTH 3" 
-names(sem[[2]])[names(sem[[2]])!=names(sem[[1]])][1]
-sem[[1]] = sem[[1]] %>% mutate(`MTH 3`=NA)  # add an empty colunm named "MTH 3"
+#------deal with file 1--I do this first because all data in the same file have similar structure
 
-## merge all data from two files
-dat=rbind(sem[[2]],sem[[3]],sem[[4]],sem[[1]])
-str(dat)  ## 636 * 60
-#------------------
-#fill in NAs
-dat %>% group_by(ID) %>% gather(key=sem)
+d1=rbind(d1_sheet[[1]],d1_sheet[[2]],d1_sheet[[3]],d1_sheet[[4]])
+d1=d1 %>% mutate(Time=replace(Time,Time=="PRE","Pre"),  ## unify upper/lower cases
+              Time=replace(Time,Time=="POST","Post"))
 
-
-#---------------------------------------
-#check consistence
-#---------------------------------------
-names(dat)
-dat %>% group_by(ID) %>% select(`GENDER.x`,`GENDER.y`) %>% ## compare gender in two files
-  gather(key=key,value=gender,`GENDER.x`:`GENDER.y`) %>% 
-  summarise(gender=unique(gender))
-## something wrong here!! Error: expecting a single value
-dat %>% group_by(ID) %>% select(`GENDER.x`,`GENDER.y`) %>%
-  gather(key=key,value=gender,`GENDER.x`:`GENDER.y`) %>% 
-  summarise(gender=length(unique(gender))) %>% arrange(desc(gender)) %>% filter(gender!=2)
-## 3 means 2 conflict gender records plus NA, 1 means missing gender
-#----------------------
-dat %>% group_by(ID) %>% select(`Characteristic.x`,`Characteristic.y`) %>%
-  gather(key=key,value=Characteristic,`Characteristic.x`:`Characteristic.y`) %>% 
-  summarise(Characteristic=unique(Characteristic))
-## something wrong here!! Error: expecting a single value
-dat %>% group_by(ID) %>% select(`Characteristic.x`,`Characteristic.y`) %>%
-  gather(key=key,value=Characteristic,`Characteristic.x`:`Characteristic.y`) %>% 
-  summarise(Characteristic=length(unique(Characteristic)))%>% 
-  arrange(desc(Characteristic)) %>% filter(Characteristic!=2)
-
-test = dat %>% filter(ID==30030)  ## ID 30030 with different gender and characteristic
-
-#-----------------------------
-dat %>% group_by(ID,test,sem) %>% filter(Time=="Pre") %>% 
-  select(1,`GENDER`:`Treatment Part 2 `)
-
-#-----------test total and normalized changes
-#split key for file1 sem1
-student = d1_sheet[[1]] %>% filter(Time=="Pre") %>% 
-  select(1,`GENDER`:`Treatment Part 2 `)
+#split key for file1 
+student1 = d1 %>% filter(Time=="Pre") %>%      ## semester/gender/charactor..whose key is only ID
+  select(1,`Normalized Changes`:`Treatment Part 2 `,sem)
  
-dat1.1 = d1_sheet[[1]] %>% select(1:12) %>%
- gather(key=answer,value=score,3:12)
+dat1 =d1 %>% select(1:`Total`,test) %>%            ## data: answer1-10...whose key is ID Time group
+  gather(key=answer ,value=score,3:12) %>% 
+  separate(answer,into=c("type","group"),sep=" ") 
+
+#-----test total and normalized changes by re-derive them--------------------
 
 ## check rederive total and normalized changes
-sumtotal=dat1.1 %>% group_by(ID,Time) %>% summarise(sum.total=sum(score)) # re-derive total
-total=d1_sheet1 %>% select(ID,Time,Total) %>% arrange(ID) ## original data
-
+sumtotal=dat1 %>% group_by(ID,Time) %>% summarise(sum.total=sum(score)) # re-derive total
+dat1.1=full_join(dat1, sumtotal, by=c("ID","Time")) 
+dat1.1 %>% filter(Total!=sum.total)  ## all right!
 
 # re-derive normalized changes (post-pre)/(100−pre) if post>pre
 ## (post-pre)/pre if post<pre
@@ -83,9 +47,84 @@ newchanges=sumtotal %>% spread(key=Time,value=sum.total) %>%
     diff=Post-Pre, 
     normalize=100-Pre, 
     normalize=replace(normalize,diff<0,Pre),
-    changes = diff/normalize)
+    changes = diff/normalize) %>%
+  select(ID,changes)
 
-d1_sheet1 %>% select(`Normalized Changes`) %>% filter(!is.na(`Normalized Changes`)) # original data
-  
+student1.1 = full_join(student1,newchanges,by="ID")
+student1.1 %>% filter(`Normalized Changes`!=changes) ## all right!
 ####----------------------------
 
+#------deal with file 2----------------------
+### File2 sem1 miss the column "MTH 3" 
+d2_sheet[[1]] = d2_sheet[[1]] %>% mutate(`MTH 3`=NA)  # add an empty colunm named "MTH 3"
+d2=rbind(d2_sheet[[2]],d2_sheet[[1]],d2_sheet[[3]],d2_sheet[[4]])
+d2=d2 %>% mutate(Time=replace(Time,Time=="PRE","Pre"),  ## unify upper/lower cases
+                 Time=replace(Time,Time=="POST","Post"))
+
+#split key for file1 
+student2 = d2 %>% filter(Time=="Pre") %>%      ## semester/gender/charactor..whose key is only ID
+  select(1,`Normalized Change`:`Treatment Part 2`,sem)
+
+dat2 =d2 %>% select(1:`Total`,test) %>%            ## data: answer1-10...whose key is ID Time group
+  gather(key=answer ,value=score,3:`DI 4`) %>% 
+  separate(answer,into=c("type","group"),sep=" ") %>%
+  mutate(score=parse_number(score))
+
+#test total and normalized changes by re-derive them
+
+## check rederive total and normalized changes
+sumtotal2=dat2 %>% group_by(ID,Time) %>% summarise(sum.total=sum(score,na.rm=T)) # re-derive total
+dat2.1=full_join(dat2, sumtotal2, by=c("ID","Time")) 
+dat2.1 %>% filter(Total!=sum.total)  ## total.score match !!!
+
+
+# re-derive normalized changes (post-pre)/(100−pre+60) if post>pre
+## (post-pre)/pre if post<pre
+newchanges2=sumtotal2 %>% spread(key=Time,value=sum.total) %>% 
+  mutate(
+    diff=Post-Pre, 
+    normalize=100-Pre+60, ### a guess from the data
+    normalize=replace(normalize,diff<0,Pre),
+    changes = diff/normalize) %>%
+  select(ID,changes)
+
+student2.1 = full_join(student2,newchanges2,by="ID")
+student2.1 %>% filter(`Normalized Change`!=changes) ## all right!
+
+#---------------------------------------
+#check consistence for gender/characteristic between two files
+#---------------------------------------
+check=full_join(student1,student2,
+          by=c("ID","sem","Treatment Part 1","Treatment Part 2 "="Treatment Part 2"))
+check %>% select(1,3:4,9:10) %>% filter(GENDER.x!=GENDER.y) 
+## genders of 10 IDs do not match !! inconsistent
+
+check %>% select(1,3:4,9:10) %>% filter(Characteristic.x!=Characteristic.y)
+## characteristics of 13 IDs do not match !! inconsistent
+
+### drop these inconsistences or change the value ??
+# try change them in file 2 and keep information from file 1
+mergekey=check %>% group_by(ID)%>% 
+  mutate(
+  gender=GENDER.x,
+  gender=replace(gender,is.na(gender),GENDER.y),
+  Characteristic = Characteristic.x,
+  Characteristic = replace(Characteristic,is.na(Characteristic),Characteristic.y)
+  ) %>%
+  select(-c(3:4,9:10)) %>% gather(key=test,value=Normalized.change,c(2,6)) %>%
+  mutate(
+    test=replace(test,test=="Normalized Changes",1),
+    test=replace(test,test=="Normalized Change",2),
+    test=parse_number(test)
+  )
+
+mergedat <- rbind(dat1,dat2) ## merge data in two files
+alldata = full_join(mergedat,mergekey,by=c("ID","test"))  ## all information form two files
+#-----------------------------
+#visual summary
+#-----------------------------
+
+x=alldata %>% group_by(ID,Time,test) %>% summarise(total=unique(Total)) %>%
+  spread(key = Time, value = total) 
+full_join(x,mergekey,by=c("ID","test")) %>% mutate(term=paste("semester",sem)) %>%
+  ggplot(aes(x=Pre,y=Post,colour=gender))+geom_point()+facet_wrap(~term)
